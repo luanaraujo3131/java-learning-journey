ce76f72e401bfef38bb53bc13575b8b5effe15d6
Luan Araújo <luanaraujo3131@icloud.com>
2023-06-13 22:14:28 -0300

    Fuck, now I think I can send

--- readme.md
+++ readme.md
@@ -1,1 +1,21 @@
-# OLÁ MUNDO
+# Estudo da linguagem java
+
+Repositório criado com o intuido de aprender git, documentar meu aprendizado e armazenar 
+meus códigos de estudo. Como ainda estou em fase de aprendizado, ele será frequentemente 
+atualizado. Algumas considerações:
+* Estou aprendendo inglês também, então há uma grande mistura entre inglés e português 
+  e perdoem o meu inglês muitas vezes chulo;
+* Meus estudos é baseado em diferentes fontes, como livros, cursos, blogs, etc. então,
+  venho trabalhado frequentemente a módularização desse repósitorio;
+---
+
+## Temas a serem abordados(roadmap):
+- Sintaxe básica
+- Tipos de dados e variáveis;
+- Condicionais;
+- Métodos(funções);
+- Loops;
+- Tratamento de exceções;
+- Orientação a Objetos, classes, interfaces...
+- Estrutura De Dados com Java
+---
--- src/Introduction/TicTacToe.java
+++ src/Introduction/TicTacToe.java
@@ -1,4 +1,5 @@
-package Tester;
+/**package Introduction;
+
 import java.util.*;
 
 public class TicTacToe {
@@ -7,10 +8,10 @@ public class TicTacToe {
 
     public static void main(String[] args){
         char[] [] gameBoard= {{' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '}};
+                             {'-', '+', '-', '+', '-'},
+                             {' ', '|', ' ', '|', ' '},
+                             {'-', '+', '-', '+', '-'},
+                             {' ', '|', ' ', '|', ' '}};
         printGameBoard(gameBoard);
 
         while (true){
@@ -41,7 +42,6 @@ public class TicTacToe {
 
             placePiece(gameBoard, cpuPosition, "CPU");
 
-
             printGameBoard(gameBoard);
 
             result = checkWinner();
@@ -50,9 +50,7 @@ public class TicTacToe {
                 break;
             }
         }
-
     }
-
     public static void printGameBoard(char[][] gameBoard){
         for (char[] row : gameBoard) {
             for (char c : row){
@@ -76,64 +74,66 @@ public class TicTacToe {
             case 1:
                 gameBoard[0][0] = symbol;
                 break;
-            case 2:
-                gameBoard[0][2] = symbol;
-                break;
-            case 3:
-                gameBoard[0][4] = symbol;
-                break;
-            case 4:
-                gameBoard[2][0] = symbol;
-                break;
-            case 5:
-                gameBoard[2][2] = symbol;
-                break;
-            case 6:
-                gameBoard[2][4] = symbol;
-                break;
-            case 7:
-                gameBoard[4][0] = symbol;
-                break;
-            case 8:
-                gameBoard[4][2] = symbol;
-                break;
-            case 9:
-                gameBoard[4][4] = symbol;
-                break;
-            default:
-                break;
-        }
-    }
-    public static String checkWinner(){
-        List topRow = Arrays.asList(1, 2, 3);
-        List midRow = Arrays.asList(4, 5, 6);
-        List botRow = Arrays.asList(7, 8, 9);
-        List leftCol = Arrays.asList(1, 4, 7);
-        List midCol = Arrays.asList(2, 5, 8);
-        List rigthCol = Arrays.asList(3, 6, 9);
-        List cross1 = Arrays.asList(1, 5, 9);
-        List cross2 = Arrays.asList(7, 5, 3);
-
-        List<List> winning = new ArrayList<>();
-        winning.add(topRow);
-        winning.add(midRow);
-        winning.add(botRow);
-        winning.add(leftCol);
-        winning.add(midCol);
-        winning.add(rigthCol);
-        winning.add(cross1);
-        winning.add(cross2);
-
-        for(List l : winning){
-            if(playerPositions.containsAll(l)){
-                return "VOCÊ GANHOU!";
-            } else if (CPUPositions.containsAll(l)) {
-                return "CPU venceu!";
-            } else if (playerPositions.size() + CPUPositions.size() == 9) {
-                return "Empate!";
+                case 2:
+                    gameBoard[0][2] = symbol;
+                    break;
+                case 3:
+                    gameBoard[0][4] = symbol;
+                    break;
+                case 4:
+                    gameBoard[2][0] = symbol;
+                    break;
+                case 5:
+                    gameBoard[2][2] = symbol;
+                    break;
+                case 6:
+                    gameBoard[2][4] = symbol;
+                    break;
+                case 7:
+                    gameBoard[4][0] = symbol;
+                    break;
+                case 8:
+                    gameBoard[4][2] = symbol;
+                    break;
+                case 9:
+                    gameBoard[4][4] = symbol;
+                    break;
+                default:
+                    break;
             }
         }
+        public static String checkWinner(){
+            List topRow = Arrays.asList(1, 2, 3);
+            List midRow = Arrays.asList(4, 5, 6);
+            List botRow = Arrays.asList(7, 8, 9);
+            List leftCol = Arrays.asList(1, 4, 7);
+            List midCol = Arrays.asList(2, 5, 8);
+            List rigthCol = Arrays.asList(3, 6, 9);
+            List cross1 = Arrays.asList(1, 5, 9);
+            List cross2 = Arrays.asList(7, 5, 3);
 
-        return "";
+            List<List> winning = new ArrayList<>();
+            winning.add(topRow);
+            winning.add(midRow);
+            winning.add(botRow);
+            winning.add(leftCol);
+            winning.add(midCol);
+            winning.add(rigthCol);
+            winning.add(cross1);
+            winning.add(cross2);
+
+            for(List l : winning){
+                if(playerPositions.containsAll(l)){
+                    return "VOCÊ GANHOU!";
+                } else if (CPUPositions.containsAll(l)) {
+                    return "CPU venceu!";
+                } else if (playerPositions.size() + CPUPositions.size() == 9) {
+                    return "Empate!";
+                }
+            }
+
+            return "";
+        }
     }
 }
+*/
